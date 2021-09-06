ae9f926d6f3036e6a75d04133fc59923cc4e74a3
MUTINTA <JustinMutinta@hotmail.com>
2021-09-05 23:19:37 -0500

    1st Pass. Plan of Action

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+Goal is to create python project that will automatically post on instagram.
+Considerations will be users to be tagged, hashtags per picture and how often to post.
+Goal is a post an hour.
+Phase 0 is to research tutorials/download the right modules for project
+Phase 1 is to be able to simply post 1 picture.
+Phase 2 is to have python get all the pictures listed in a folder and add them to a list.
+Phase 3 is to have the contents of a txt document (tags) be read by python and added to a list.
+Phase 4 is to have python be able to upload a picture, wait 5 minutes and then post another picture.
+Phase 5 is to have python get photo/tag information, and then be able to post once an hour.
+"""
+
